324d3cd1ecb592e76dee4b815cb752addea47dc5
iqiqiya <iqiqiya@outlook.com>
2019-07-29 14:21:25 +0800

    新增千图网58pic无水印解析api
    新增千图网58pic无水印解析api

---
+++ 58pic/README.md
@@ -0,0 +1,3 @@
+## 58pic(千图网无水印解析下载)
+### 调用方式如下
+http://127.0.0.1/api.php?PicUrl=https://www.58pic.com/newpic/34816843.html
---
+++ 58pic/index.php
@@ -0,0 +1,18 @@
+<?php
+function get58Pic($PicUrl) {
+    $contents = file_get_contents($PicUrl);
+    preg_match("~preview(.*?).jpg~", $contents, $matches);
+    if (count($matches) == 0) {
+        echo '无法转换成相应的无水印图片，请换个链接试一下。';
+        exit;
+    }
+    $img_url = $matches[1];
+    $img_url_no_watermark = "http://pic".$img_url.".jpg";
+    //echo $img_url_no_watermark;    //输出img_url
+    header("Location: $img_url_no_watermark");    //header跳转
+}
+$Pic_Url = $_GET['PicUrl'];
+//$Pic_Url = "https://www.58pic.com/newpic/34816843.html";
+get58Pic($Pic_Url);
+exit; 
+?>
