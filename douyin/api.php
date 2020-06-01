6512065a4ae5c50435fc798464e61c8a7948e038
iqiqiya <43666095+iqiqiya@users.noreply.github.com>
2020-06-01 10:34:30 +0800

    更新抖音
    抖音去水印更新

--- douyin/api.php
+++ douyin/api.php
@@ -8,70 +8,61 @@
  */
 
 error_reporting(0);
-//获取302跳转后的url
-function get_redirect_url($url) {
+function get_content_url($url) {
     $ch = curl_init();
+    //设置请求头
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4'
+    ));
     curl_setopt($ch, CURLOPT_URL, $url);
-    // 不需要页面内容
-    curl_setopt($ch, CURLOPT_NOBODY, 1);
-    // 不直接输出
-    curl_setopt($ch, CURLOPT_USERAGENT,"Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    // 返回最后的Location
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_exec($ch);
-    $info = curl_getinfo($ch,CURLINFO_EFFECTIVE_URL);
-    curl_close($ch);
-    return $info;
+    //启用时会将头文件的信息作为数据流输出
+    curl_setopt($ch, CURLOPT_HEADER, False);
+    //设置为FALSE 禁止 cURL 验证对等证书
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, False);
+    //获取页面内容 不直接输出到页面
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //设置最大超时时间
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5000);
+    $result = curl_exec($ch);
+    return $result;
 }
-function get_down_url($url){
-    ini_set('user_agent', 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1');
-    return file_get_contents($url);
+function zz_get_url($content){
+    preg_match('/href="(.*?)">Found/', $content, $matches);
+    $res = $matches[1];
+    return $res;
 }
-function zz_get_dytk($content){
-    preg_match("~dytk(.*?)}~", $content, $matches);
-    $Dytk = $matches[1];
-    preg_match("~\"(.*?)\"~",$Dytk,$matches2);
-    $dytk = $matches2[1];
-    return $dytk;
+function zz_item_id($content){
+    preg_match('/itemId: "(.*?)",/', $content, $matches);
+    $res = $matches[1];
+    return $res;
 }
-function zz_get_item_ids($content){
-    preg_match("~video/(.*?)/~", $content, $matches);
-    $item_ids = $matches[1];
-    return $item_ids;
-}
-function get_url($url){
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $output = curl_exec($ch);
-    curl_close($ch);
-    $output = json_decode($output,true);
-    return $output;
-}
+
 function zz_video_url($content){
     preg_match("~https(.*?)\"~", $content, $matches);
     $item_ids = $matches[0];
     return $item_ids;
 }
 
-$url = $_POST['url'];
+//$url = $_GET['url'];
+$url = "http://v.douyin.com/xGSE7P/";
 $str_r= '/(http:\/\/|https:\/\/)((\w|=|\?|\.|\/|&|-)+)/';
 preg_match_all($str_r,$url,$arr);
 $share_url=$arr[0][0];
 
-//$share_url = "http://v.douyin.com/xGSE7P/";
-$url_302 =  get_redirect_url($share_url);//打印跳转后的链接
+$content1 =  get_content_url($share_url);//获得源码
+$url_302 = zz_get_url($content1);//正则拿到跳转后的url
 
-$mid = zz_get_item_ids($url_302);//匹配mid
+$content2 = get_content_url($url_302);
+$item_id = zz_item_id($content2);
 
-$content1 =  get_down_url($url_302);//获得源吗
-$dytk = zz_get_dytk($content1);//匹配dytk
+$url_pj = "https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids=".$item_id;//拼接url
+$content3 = json_decode(get_content_url($url_pj),true);
 
-$url_2 = "https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids=".$mid."&dytk=".$dytk;//拼接最后的url
-$result = file_get_contents($url_2);
-header('Content-Type:application/json; charset=utf-8');
-print_r($result);
+$watermark_url = $content3['item_list'][0]['video']['play_addr']['url_list'][0];
+$no_watermark_url = str_replace("playwm","play",$watermark_url);//这个得到的地址需要更改UA才可以正常显示
+
+$content4 = get_content_url($no_watermark_url);
+$youhua_url = zz_get_url($content4);
+print_r($youhua_url);
+
 ?>
